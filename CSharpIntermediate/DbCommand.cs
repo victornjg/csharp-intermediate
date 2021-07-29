bccd5d09793d28e0c88172c2a305116ba0f0941d
victornjg <victor.nnjg@gmail.com>
2021-07-29 09:54:30 -0300

    solution for the db command exercise

---
+++ CSharpIntermediate/DbCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    partial class Program
+    {
+        public class DbCommand
+        {
+            private readonly DbConnection _dbConnection;
+            private readonly string _instruction;
+
+            public DbCommand(DbConnection dbConnection, string instruction)
+            {
+                if (dbConnection == null)
+                    throw new ArgumentNullException("Provide a valid instance of DbConnection.");
+                if (string.IsNullOrWhiteSpace(instruction))
+                    throw new ArgumentException("Provide a valid instruction.");
+                this._dbConnection = dbConnection;
+                this._instruction = instruction;
+            }
+
+            public void Execute()
+            {
+                this._dbConnection.Open();
+                Console.WriteLine(_instruction);
+                Console.WriteLine("Running instruction...");
+                this._dbConnection.Close();
+            }
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -6,14 +6,11 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            var sqlConnection = new SqlConnection("mysqldb;123456", TimeSpan.FromMinutes(1));
+            //var sqlConnection = new SqlConnection("mysqldb;123456", TimeSpan.FromMinutes(1));
             var oracleConnection = new OracleConnection("myoracledb;abcdef", TimeSpan.FromMinutes(2));
 
-            sqlConnection.Open();
-            sqlConnection.Close();
-
-            oracleConnection.Open();
-            oracleConnection.Close();
+            var dbCommand = new DbCommand(oracleConnection, "select * from test");
+            dbCommand.Execute();       
         }
     }
 }
